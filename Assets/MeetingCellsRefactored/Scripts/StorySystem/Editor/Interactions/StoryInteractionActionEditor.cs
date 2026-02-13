8853e38dfa33fe5eb9adb78dc822383b668ed0ed
CatfishW <1819409756@qq.com>
2026-02-12 19:50:24 -0500

    Add modular interaction pipeline with story event blending and UI Toolkit editors

---
+++ Assets/MeetingCellsRefactored/Scripts/StorySystem/Editor/Interactions/StoryInteractionActionEditor.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:424631c0a351b1ee81d8071d4a9c6deb9e8d3baa2922753505ff6556e984b47e
+size 25406
---
+++ Assets/MeetingCellsRefactored/Scripts/StorySystem/Runtime/Interactions/InteractionPoint.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3439f96e461e5238c20d60f83b2fb83cf0fa62e4557b4c27f9661251f95c08f0
+size 11694
