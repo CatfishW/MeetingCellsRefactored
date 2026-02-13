629eb5859b073a2bba9571d2c950493175f653cf
CatfishW <1819409756@qq.com>
2026-02-12 19:56:13 -0500

    Fix UI Toolkit spacing API usage in interaction editors

--- Assets/MeetingCellsRefactored/Scripts/StorySystem/Editor/Interactions/InteractionPointEditor.cs
+++ Assets/MeetingCellsRefactored/Scripts/StorySystem/Editor/Interactions/InteractionPointEditor.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:daca43b5eb2f7c44ba28d5674086131d1fb025196af69669fcfd318c13dbcc15
-size 8196
+oid sha256:7857f4d443664a1b6b323bd87ae2322dc86b438be0ba947ba343085245646085
+size 8170
